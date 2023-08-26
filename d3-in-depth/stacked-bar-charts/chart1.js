87644a8369313b9a7f7327aa307639b1778b15f2
boa50 <bruno.albuquerque50@gmail.com>
2023-08-26 14:34:28 +0930

    Created a basic stacked bar chart

---
+++ d3-in-depth/stacked-bar-charts/chart1.js
@@ -0,0 +1,52 @@
+const chart1 = () => {
+    getData = async () => await d3.csv('./dataset1.csv')
+
+    const svg = getSvg(1, 'Basic stacked')
+
+    getData().then(data => {
+        const groups = d3.map(data, d => d.group)
+        const subgroups = data.columns.slice(1)
+
+        xScale = d3
+            .scaleBand()
+            .domain(groups)
+            .range([20, WIDTH])
+            .padding([0.2])
+        svg.append('g')
+            .attr('transform', `translate(0, ${HEIGHT - 20})`)
+            .call(d3.axisBottom(xScale).tickSizeOuter(0))
+
+        yScale = d3
+            .scaleLinear()
+            .domain([0, 60])
+            .range([HEIGHT - 20, 0])
+        svg.append("g")
+            .attr('transform', `translate(20, 0)`)
+            .call(d3.axisLeft(yScale));
+
+        const colours = d3
+            .scaleOrdinal()
+            .domain(subgroups)
+            .range(d3.schemeTableau10)
+
+        const stackedData = d3
+            .stack()
+            .keys(subgroups)
+            (data)
+
+        svg
+            .selectAll('.group')
+            .data(stackedData)
+            .join('g')
+            .attr('fill', d => colours(d.key))
+            .selectAll('rect')
+            .data(d => d)
+            .join('rect')
+            .attr('x', d => xScale(d.data.group))
+            .attr('y', d => yScale(d[1]))
+            .attr('height', d => yScale(d[0]) - yScale(d[1]))
+            .attr('width', xScale.bandwidth())
+    })
+}
+
+chart1()
