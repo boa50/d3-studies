72801e82a1cc6452c699ec732d252ad968cdf582
boa50 <bruno.albuquerque50@gmail.com>
2023-08-27 16:40:09 +0930

    Created a basic pie chart

---
+++ d3-in-depth/pie-charts/chart1.js
@@ -0,0 +1,32 @@
+const chart1 = () => {
+    const data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
+    const svg = getSvg(1, 'Basic pie chart')
+
+    const radius = Math.min(WIDTH, HEIGHT) / 2
+
+    const colours = d3
+        .scaleOrdinal()
+        .range(d3.schemeTableau10)
+
+    const pie = d3
+        .pie()
+        .value(d => d[1])
+    const data_ready = pie(Object.entries(data))
+
+    const arcGenerator = d3
+        .arc()
+        .innerRadius(0)
+        .outerRadius(radius)
+
+    svg
+        .append('g')
+        .attr('transform', `translate(${WIDTH / 2}, ${HEIGHT / 2})`)
+        .selectAll('whatever')
+        .data(data_ready)
+        .join('path')
+        .attr('d', arcGenerator)
+        .attr('fill', d => colours(d.data[1]))
+        .style('opacity', 0.7)
+}
+
+chart1()
